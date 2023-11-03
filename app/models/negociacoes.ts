ffcd4939ae8a2c5d7823b80e47770e01b5cd4425
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2023-11-03 18:01:00 -0300

    Simplificando o código e adicionando programação defensiva

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,6 +3,10 @@ import { Negociacao } from "./negociacao";
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
 
+    /*
+        outra forma de declarar um array de negociacao é usar Negocicacao[]
+    */ 
+
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
@@ -10,4 +14,9 @@ export class Negociacoes {
     lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes
     }
+
+    /*
+        Outra forma de declarar a função de array readyonly é:
+        lista(): readyonly Negociacao[] {
+    */ 
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,7 +1,14 @@
 export class Negociacoes {
     constructor() {
         this.negociacoes = [];
+        /*
+            Outra forma de declarar a função de array readyonly é:
+            lista(): readyonly Negociacao[] {
+        */
     }
+    /*
+        outra forma de declarar um array de negociacao é usar Negocicacao[]
+    */
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
